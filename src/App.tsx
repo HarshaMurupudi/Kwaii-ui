d0d92841ca3508fd5a3c572b69993e0905a4c01a
harsha Murupudi <itsmeeeharsha@gmail.com>
2020-11-30 08:23:44 +0530

    ADD: Basic router imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import SmileyPage from './pages/smiley/smiley-page.component';
 
 function App() {
   return (
     <div className="App">
+      {/* Header */}
       <SmileyPage />
+      {/* Footer */}
     </div>
   );
 }
