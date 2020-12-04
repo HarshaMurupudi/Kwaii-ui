414cc9b87ac03fb8adf442eb75ab1c5cec18d83d
harsha Murupudi <itsmeeeharsha@gmail.com>
2020-12-04 08:21:26 +0530

    ADD: Global colors config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import "./styles/style.scss";
+import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
