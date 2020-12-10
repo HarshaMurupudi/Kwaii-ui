6373b77bd7e7af22ae3f0f2fd0e15ec8133d18f5
harsha Murupudi <itsmeeeharsha@gmail.com>
2020-12-10 06:50:57 +0530

    ADD: Sidedrawer to home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import SmileyPage from './pages/smiley/smiley-page.component';
+import HomePage from './pages/home-page/home-page.component';
 
 function App() {
   return (
     <div className="App">
       {/* Header */}
-      <SmileyPage />
+      {/* <SmileyPage /> */}
+      <HomePage />
       {/* Footer */}
     </div>
   );
--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,1 +1,11 @@
 import React from 'react'
+
+import SideDrawer from '../../components/side-drawer/side-drawer.component';
+
+const HomePage: React.FC = () => {
+  return (
+    <SideDrawer />
+  )
+}
+
+export default HomePage;
