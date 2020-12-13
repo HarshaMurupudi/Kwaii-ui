a8be0107ac6135d1edc16ea47a99301a33caac2c
harsha Murupudi <itsmeeeharsha@gmail.com>
2020-12-13 06:48:40 +0530

    ADD: Theme file for creating new mui theme

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+export default theme;
---
+++ src/styles/variables.js
@@ -0,0 +1,29 @@
+const lightgreen = '#5bd7b9';
+const green = '#17a589';
+const darkgreen = '#00755c';
+
+const lightblue = '#516171';
+const blue = '#273746';
+const darkblue = '#00111f';
+
+const lightorange = '#ffc947';
+const orange = '#ff9800';
+const darkorange = '#c66900';
+
+const black = '#000000';
+const white = '#ffffff';
+
+// $color - primary: $blue;
+// $color - secendory: $orange;
+// $color - text - primary: $white;
+// $color - text - secondary: $black;
+// $color - surface: $white;
+// $color - background: $white;
+
+
+const varibles = {
+
+
+}
+
+export default varibles;
