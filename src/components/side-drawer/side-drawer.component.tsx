c4e9cf0b5d745a57200b6577c66ee3ba08000922
harsha Murupudi <itsmeeeharsha@gmail.com>
2020-12-18 07:12:58 +0530

    CHANGE: Material icon import fortmat in side drawer for cleaner look

--- src/components/side-drawer/side-drawer.component.tsx
+++ src/components/side-drawer/side-drawer.component.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+
 import clsx from 'clsx';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import IconButton from '@material-ui/core/IconButton';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Drawer, CssBaseline, AppBar, Toolbar, List, Typography, Divider, ListItem, IconButton, ListItemIcon, ListItemText } from '@material-ui/core';
+
 import MenuIcon from '@material-ui/icons/Menu';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
