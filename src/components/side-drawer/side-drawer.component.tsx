94555c7f12ad479f268e31d199682c7f2126f355
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-01-01 10:19:49 +0530

    TEST: Commit

--- src/components/side-drawer/side-drawer.component.tsx
+++ src/components/side-drawer/side-drawer.component.tsx
@@ -46,14 +46,6 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
-  // appBarShift: {
-  //   marginLeft: drawerWidth,
-  //   width: `calc(100% - ${drawerWidth}px)`,
-  //   transition: theme.transitions.create(['width', 'margin'], {
-  //     easing: theme.transitions.easing.sharp,
-  //     duration: theme.transitions.duration.enteringScreen,
-  //   }),
-  // },
   menuButton: {
     marginRight: 36,
   },
