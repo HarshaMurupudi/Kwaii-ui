6e76d2ab02337c07b45343bd332e4647b226568e
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-01-15 14:39:18 +0530

    ADD: Basic text to home page

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -5,7 +5,8 @@ const HomePage: React.FC = (props) => {
 
   return (
     <div>
-
+      <h4>Kawaii UI</h4>
+      <p>Find cute and stylish components that strike a fun and awsome experiece</p>
 
     </div>
   )
