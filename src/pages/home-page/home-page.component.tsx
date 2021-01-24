772e4bd44965bd31a8643e2e82ab04d25c441ab6
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-01-24 17:34:41 +0530

    ADD: Content in the spash screen/home page

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import clsx from 'clsx';
 import {
-  Typography
+  Typography,
+  Button,
+  Container
 } from '@material-ui/core';
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 
@@ -91,29 +93,18 @@ const HomePage = ({ isDrawerOpen }: PropsType) => {
       <div className={isDrawerOpen ? classes.shiftTextLeft : classes.shiftTextLeftMini} >
 
         <div className={classes.drawerHeader} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Container maxWidth="sm">
+          <h1> Kawaii UI</h1>
+          <p>Cute UI elements for the web</p>
+          <div>
+            <Button variant="outlined" color="primary">
+              Get Started
+          </Button>
+            <Button variant="outlined" color="primary">
+              Latest
+          </Button>
+          </div>
+        </Container>
       </div>
     </main>
   )
