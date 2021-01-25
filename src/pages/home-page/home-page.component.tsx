7199717a2ce20f29e03472b0462fe4c8874c3b1d
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-01-25 08:02:23 +0530

    CLEAN: Rearrange styles in home page

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -11,76 +11,9 @@ interface PropsType {
   isDrawerOpen: boolean
 }
 
+const drawerWidth = 240;
+
 const HomePage = ({ isDrawerOpen }: PropsType) => {
-
-  const drawerWidth = 240;
-
-  const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        display: 'flex',
-      },
-      appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-      },
-      appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-      menuButton: {
-        marginRight: theme.spacing(2),
-      },
-      hide: {
-        display: 'none',
-      },
-      drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
-      drawerPaper: {
-        width: drawerWidth,
-      },
-      drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-      },
-      content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-      },
-      contentShift: {
-        transition: theme.transitions.create('margin', {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-      },
-      shiftTextLeft: {
-        // understand close width
-        marginLeft: drawerWidth
-      },
-      shiftTextLeftMini: {
-        // open width + mini width
-        marginLeft: 240 + 73
-      },
-    }),
-  );
 
   const classes = useStyles();
 
@@ -95,7 +28,7 @@ const HomePage = ({ isDrawerOpen }: PropsType) => {
         <div className={classes.drawerHeader} />
         <Container maxWidth="sm">
           <h1> Kawaii UI</h1>
-          <p>Cute UI elements for the web</p>
+          <p>Simple and Cute UI components for the web</p>
           <div>
             <Button variant="outlined" color="primary">
               Get Started
@@ -109,5 +42,45 @@ const HomePage = ({ isDrawerOpen }: PropsType) => {
     </main>
   )
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+    shiftTextLeft: {
+      // understand close width
+      marginLeft: drawerWidth
+    },
+    shiftTextLeftMini: {
+      // open width + mini width
+      marginLeft: 240 + 73
+    },
+  }),
+);
 
 export default HomePage;
