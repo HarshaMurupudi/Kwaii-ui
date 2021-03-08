072cc2ab526959f254c69843d98a326efbec9bbf
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-03-08 09:13:33 +0530

    ADD: Comments for ripple effect execution

--- src/components/button/simple-btn.component.tsx
+++ src/components/button/simple-btn.component.tsx
@@ -3,8 +3,35 @@ import React from 'react'
 import './simple-btn.styles.scss'
 
 const SimpleBtn: React.FC = () => {
+
+  const handleClick = () => {
+    // // Create span element 
+    // let ripple = document.createElement("span");
+
+    // // Add ripple class to span 
+    // ripple.classList.add("ripple");
+
+    // // Add span to the button  
+    // this.appendChild(ripple);
+
+    // // Get position of X 
+    // let x = e.clientX - e.target.offsetLeft;
+
+    // // Get position of Y  
+    // let y = e.clientY - e.target.offsetTop;
+
+    // // Position the span element 
+    // ripple.style.left = `${x}px`;
+    // ripple.style.top = `${y}px`;
+
+    // // Remove span after 0.3s 
+    // setTimeout(() => {
+    //   ripple.remove();
+    // }, 300);
+  }
+
   return (
-    <button className="btn">
+    <button className="btn" onClick={handleClick}>
       Enter
     </button>
   )
