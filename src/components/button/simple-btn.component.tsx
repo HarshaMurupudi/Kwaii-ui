7c1d90aff08b73c8097b14ce17abfd9118bd1ca7
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-03-09 09:23:54 +0530

    CHANGE: Method for implimenting ripple effect on button

--- src/components/button/simple-btn.component.tsx
+++ src/components/button/simple-btn.component.tsx
@@ -2,39 +2,24 @@ import React from 'react'
 
 import './simple-btn.styles.scss'
 
-const SimpleBtn: React.FC = () => {
-
-  const handleClick = () => {
-    // // Create span element 
-    // let ripple = document.createElement("span");
-
-    // // Add ripple class to span 
-    // ripple.classList.add("ripple");
-
-    // // Add span to the button  
-    // this.appendChild(ripple);
-
-    // // Get position of X 
-    // let x = e.clientX - e.target.offsetLeft;
-
-    // // Get position of Y  
-    // let y = e.clientY - e.target.offsetTop;
-
-    // // Position the span element 
-    // ripple.style.left = `${x}px`;
-    // ripple.style.top = `${y}px`;
-
-    // // Remove span after 0.3s 
-    // setTimeout(() => {
-    //   ripple.remove();
-    // }, 300);
+class SimpleBtn extends React.Component {
+  initializeState = () => {
+    return {
+      spanStyles: {},
+      count: 0
+    }
   }
+  state = this.initializeState();
+  render() {
+    // const { children = null, classes = "", onClickHandler = null } = this.props;
+    return (
+      // <div className={classes} onClick={this.onClickHandler}>
+      //   {children}
+      // </div>
 
-  return (
-    <button className="btn" onClick={handleClick}>
-      Enter
-    </button>
-  )
+      <></>
+    );
+  }
 }
 
 export default SimpleBtn;
