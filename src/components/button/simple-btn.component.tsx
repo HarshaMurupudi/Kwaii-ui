f0dbfc7e35b6fca769542ba908a93b0fb46fc7d0
harsha Murupudi <itsmeeeharsha@gmail.com>
2021-03-17 09:06:07 +0530

    ADD: Ripple logic structure to simple btn file

--- src/components/button/simple-btn.component.tsx
+++ src/components/button/simple-btn.component.tsx
@@ -16,12 +16,64 @@ class SimpleBtn extends React.Component<PropType> {
     }
   }
   state = this.initializeState();
+
+  cleanUp = () => {
+    const initialState = this.initializeState();
+    this.setState({ ...initialState });
+  }
+
+  showRipple = (e: any) => {
+    const rippleContainer = e.currentTarget;
+    const size = rippleContainer.offsetWidth;
+    const pos = rippleContainer.getBoundingClientRect();
+    const x = e.pageX - pos.x - (size / 2);
+    const y = e.pageY - pos.y - (size / 2);
+
+    const spanStyles = { top: y + 'px', left: x + 'px', height: size + 'px', width: size + 'px' };
+    const count = this.state.count + 1;
+    this.setState({
+      spanStyles: { ...this.state.spanStyles, [count]: spanStyles },
+      count: count
+    });
+  }
+
+  /* Debounce Code to call the Ripple removing function */
+  callCleanUp = (cleanup: any, delay: any) => {
+    return function () { }
+
+    // return function () {
+    //   clearTimeout(this.bounce);
+    //   this.bounce = setTimeout(() => {
+    //     cleanup();
+    //   }, delay);
+    // }
+  }
+
+  renderRippleSpan = () => {
+    return <div></div>
+    // const { showRipple = false, spanStyles = {} } = this.state;
+    // const spanArray = Object.keys(spanStyles);
+    // if (spanArray && spanArray.length > 0) {
+    //   return (
+    //     spanArray.map((key, index) => {
+    //       return <span key={'spanCount_' + index} className="" style={{ ...spanStyles[key] }}></span>
+    //     })
+    //   )
+    // } else {
+    //   return null;
+    // }
+  }
+
+
   render() {
     const { children = null, classes = "", onClickHandler } = this.props;
     return (
       <div>
         <div ref="targetElement" className={'ripple ' + classes} onClick={onClickHandler}>
           {children}
+          <div className="rippleContainer" onMouseDown={this.showRipple} onMouseUp={this.callCleanUp(this.cleanUp, 2000)}>
+            {this.renderRippleSpan()}
+          </div>
         </div>
       </div>
     );
